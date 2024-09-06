c064ee60fc2ac6f878fea26488ea9d81412ae592
Ibrahim Fradj <Fradj.ibrahim@gmail.com>
2024-09-06 19:25:32 -0400

    Add homepage files

---
+++ ibrahim-fradj/src/components/homepage/Homepage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Homepage.css'
+
+function Homepage() {
+  return (
+    <div className="homepage">
+      <h1>Bienvenue sur ma page d'accueil</h1>
+    </div>
+  )
+}
+
+export default Homepage
